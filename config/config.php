ae1c21a78af76f55042c28959b2a85c1704926b7
Ethan <flymazda@163.com>
2017-05-23 17:56:51 +0800

    first commit

---
+++ config/config.php
@@ -0,0 +1,14 @@
+<?php
+//database config
+define('DB_NAME','fastphp');
+define('DB_USER','root');
+define('DB_PASSWORD','');
+define('DB_HOST','localhost');
+
+
+//default controllers and default action
+$config['defaultController']= 'Item';
+$config['defaultAction']='index';
+
+return $config;
+
