2ee6e34dd0019a7e3eba3116aa176a7de0f2a25b
ethan <flymazda@163.com>
2017-06-14 16:18:16 +0800

    mvc基本完成

--- fastphp/Model.php
+++ fastphp/Model.php
@@ -6,8 +6,15 @@ class Model extends Medoo{
 
     public function __construct(){
         // 连接数据库 
-        parent::__construct(array(self::$dbConfig['database_type'], self::$dbConfig['database_name'], self::$dbConfig['server'],
-            self::$dbConfig['username'],self::$dbConfig['password']));
+        $server =array(
+        		"database_type"=>self::$dbConfig['database_type'],
+        		"database_name"=>self::$dbConfig['database_name'],
+        		"server"=>self::$dbConfig['server'],
+        		"username"=>self::$dbConfig['username'],
+        		"password"=>self::$dbConfig['password']
+        	); 
+        			
+        parent::__construct($server);
     }
 
 }
